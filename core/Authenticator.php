747c0eba86d213fe3862912f75e1113581704e90
evan bittar <evanking621@gmail.com>
2025-10-14 12:47:47 -0300

    add class  Authenticator but not finish yet

--- core/Authenticator.php
+++ core/Authenticator.php
@@ -1,11 +1,30 @@
 <?php 
 
+namespace Core;
+
 use core\Database;
+
+$config = require base_path('config.php');
 
 class Authenticator{
 
-    public function auth($email, $password){
-        $user = 
+    public $db = new Database($config);
 
+    public function auth($email, $password){
+    $user = $this->db->query("SELECT * FROM users WHERE email = :email",[
+    'email'=>$email
+    ])->findOrFales();
+    if ($user && password_verify($password, $user['password'])){
+        $this->login([
+            'email'=>$user['email'],
+            'id'=>$user['id']
+        ]);
+    }
 }
+    public function login($user){
+    $_SESSION['user'] =[
+    'email'=>$user['email'],
+    'id'=>$user['id']
+    ];
+    }
 }
